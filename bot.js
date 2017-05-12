44329c260d119ca6205d1c2fd7708d65a17ad135
miakeous <tardiveau.pierre@telecom-st-etienne.fr>
2017-05-12 15:07:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 //
 // This is main file containing code implementing the Express server and functionality for the Express echo bot.
 //
+
+
+
+//Déclaration des variables
+
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,6 +14,7 @@ const request = require('request');
 const path = require('path');
 var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<footer id=\"gWidget\"></footer><script src=\"https://widget.glitch.me/widget.min.js\"></script></body></html>";
 var res;
+
 // The rest of the code implements the routes for our Express server.
 let app = express();
 
@@ -16,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 
 // Webhook validation
 app.get('/webhook', function(req, res) {
@@ -29,14 +36,18 @@ app.get('/webhook', function(req, res) {
   }
 });
 
-// Display the web page
+
+// Display the web page truc qui sert a rien
 app.get('/', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(messengerButton);
   res.end();
 });
 
-// Message processing
+
+
+
+// Fonction qui va recevoir les events et les analyser pour réaliser les actions a faire
 app.post('/webhook', function (req, res) {
   console.log(req.body);
   var data = req.body;
@@ -60,17 +71,16 @@ app.post('/webhook', function (req, res) {
         }
       });
     });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
     res.sendStatus(200);
   }
 });
 
-// Incoming events handling
+
+
+
+
+
+// Reçois les informations de la foncion précedentes et analyse le message
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -93,22 +103,17 @@ function receivedMessage(event) {
   },
   "sender_action":"typing_on"
 };
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the template example. Otherwise, just echo the text we received.
+    // fonction qui permet d'afficher les 3 points quand le bot répond (sert a montrer à l'utilisateur que le bot va lui répondre)
+    
     callSendAPI(messageData);
     switch (messageText) {
       case 'generic':
-        
         sendGenericMessage(senderID);
         break;
       case 'product':
-        
         sendProductMessage(senderID);
         break;
-      
-
       default:
-        
         sendTextMessage(senderID, messageText);
     }
   } else if (messageAttachments) {
@@ -116,6 +121,9 @@ function receivedMessage(event) {
   }
 }
 
+
+
+// Fonction qui reçoit les callback et qui va les analyser
 function receivedPostback(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -126,21 +134,13 @@ function receivedPostback(event) {
   },
   "sender_action":"typing_on"
 };
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
   var payload = event.postback.payload;
   payload =payload.split('||');
   console.log(payload[1]);
   console.log("Received postback for user %d and page %d with payload '%s' " + 
     "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  callSendAPI(messageData);
+   callSendAPI(messageData);
   switch(payload[0]){
-    /*case 'product' :
-      sendProductMessage(senderID);
-      break;*/
       case 'getStarted' :
       sendGenericMessage(senderID);
       break;
@@ -153,17 +153,22 @@ function receivedPostback(event) {
       case 'information' :
       sendProductMessage(senderID, payload[1]);
       break;
-      
+      case 'nutri':
+      sendNutriMessage(senderID,payload[1]);
+      break;
       default:
         sendTextMessage(senderID, "postBack called");break
-      
-                
-  
-}}
+                   }}
+
+
+
+
+
 
 //////////////////////////
-// Sending helpers
+// Fonction va envois un message de type Text des plus classique
 //////////////////////////
+
 function sendTextMessage(recipientId, messageText) {
   var messageData = {
     recipient: {
@@ -173,67 +178,70 @@ function sendTextMessage(recipientId, messageText) {
       text: messageText
     }
   };
-
   callSendAPI(messageData);
 }
 
+
+
+
+
+//////////////////////////
+// Fonction va envois un message de type Générique (Ex : affichage des collections par exemple)
+//////////////////////////
+
 function sendGenericMessage(recipientId) {
   var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "Le bon paté",
-            subtitle: "Site Paté",
-            item_url: "https://fr.ulule.com/chambouletou-pate/",               
-            image_url: "https://drfhlmcehrc34.cloudfront.net/cache/a/7/41941855651c35858f7e51de7be9e8.jpg",
-            buttons: [{
-              type: "web_url",
-              url: "https://tsechat.myshopify.com/?key=c9fe91dcceba2aa7825134da9eef9b648478eb7443dcb118b0048fb2e543fda2",
-              title: "Ouvrir le site"
-            }, {
-              type: "postback",
-              title: "Voir tout les produit",
-              payload: "collection",
-            }]
-          }]
-        }
+  "recipient":{
+    "id":recipientId
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"Bonjour, \nPermettez moi de vous guider.\nQue voulez vous faire ?",
+        "buttons":[
+          {
+            "type":"web_url",
+            "url":"https://tsechatbot.myshopify.com",
+            "title":"Ouvrir le site"
+          },
+          {
+            "type":"postback",
+            "title":"Voir nos collections ",
+            "payload":"collection"
+          },{
+            "type":"web_url",
+            "url":"https://tsechatbot.myshopify.com",
+            "title":"FAQ",
+            
+          }
+        ]
       }
     }
-  };  
-  
+  }
+};  
   console.log(messageData.message.attachment.payload.elements);
   callSendAPI(messageData);
 }
 
 
-function sendProductMessage(recipientId,name) {
-  var uri = "https://tsechat.myshopify.com/admin/products.json?collection_id="+name;
-  console.log(uri);
-  getProduit(uri,recipientId,readData);
-  //console.log(JSON.stringify(resu));
-  //var table = JSON.parse(getJSON('https://tsechat.myshopify.com/admin/products.json'));
-  //console.log(table);
-  
-  
-//callSendAPI(messageData);
-}
 
-function sendCollectionMessage(recipientId){
-  getCollection('https://tsechat.myshopify.com/admin/custom_collections.json',recipientId,console.log);
+
+//////////////////////////
+// Fonction va aller chercher des informations sur shopify (produits en fonction de l'id de la collection)
+//////////////////////////
+
+function sendProductMessage(recipientId,name) {
+  var uri = "https://tsechatbot.myshopify.com/admin/products.json?collection_id="+name;
+  console.log(uri);
+  getProduit(uri,recipientId,name);
 }
-  
-  
-function getProduit(url,recipientId,callback) {
+function getProduit(url,recipientId,name) {
   
     var xhr = new XMLHttpRequest();
   
-    xhr.open('GET', url, true,"f56f085bf6ee7fd32781433d10a20845","b7f82c376582006f01256d71657a02e3");
+    xhr.open('GET', url, true,"62ffda2cd474e07070563fe4089c443f","6295eda9c2499abe83a342338e7b5562");
     xhr.responseType = 'json';
     xhr.onload = function() {
       var status = xhr.status;
@@ -241,42 +249,129 @@ function getProduit(url,recipientId,callback) {
         
         res = JSON.parse(xhr.responseText);
         console.log(res);
-        console.log("bondour");
-        creationmessage(res,recipientId);
+        //console.log("bondour");
+        creationmessage(res,recipientId,name);
         
       } else {
           console.log(status);
-        //callback(status);
+
       }
     };
     xhr.send();
 };
 
-function getCollection(url,recipientId,callback) {
+
+
+
+
+
+//////////////////////////
+// Fonction va aller chercher des informations sur shopify (Collection)
+//////////////////////////
+
+function sendCollectionMessage(recipientId){
+  getCollection('https://tsechatbot.myshopify.com/admin/custom_collections.json',recipientId,console.log);
+}
+function getCollection(url,recipientId) {
 
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true,"f56f085bf6ee7fd32781433d10a20845","b7f82c376582006f01256d71657a02e3");
+    xhr.open('GET', url, true,"62ffda2cd474e07070563fe4089c443f","6295eda9c2499abe83a342338e7b5562");
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        res = JSON.parse(xhr.responseText);
+        console.log(res);
+        creationliste(res,recipientId);
+       } 
+    };
+    xhr.send();
+};
+function creationliste(data,recipientId){
+  var k = 0;
+  var message;
+  var messageData = {
+  "recipient":{
+    "id":recipientId
+  }, "message": {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [
+            ]  
+        }
+    }
+}
+    
+};
+  for(var t in data.custom_collections){
+    //console.log(data.custom_collections);
+    message =    {
+                    "title": data.custom_collections[k].title,
+                    "image_url": data.custom_collections[k].image.src,
+                    "subtitle": data.custom_collections[k].body_html,
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://tsechatbot.myshopify.com/collections/"+data.custom_collections[k].handle,
+                        },
+                    "buttons": [{
+                      "title": "Voir plus",
+                    "type": "postback",
+                    "payload": "information||"+ data.custom_collections[k].id,
+                       }
+                    ]
+                };
+    messageData.message.attachment.payload.elements[k] = message;
+    k++;
+  }
+  //console.log(messageData);
+  //console.log(messageData.message.attachment.payload);
+  callSendAPI(messageData);
+}
+
+
+
+//////////////////////////
+// Fonction va aller chercher des informations sur shopify (Metafields sur la nutrition en fonction du Handle du produit)
+//////////////////////////
+
+function sendNutriMessage(recipientID,name){
+ getNutri('https://tsechatbot.myshopify.com/admin/products/'+name+'/metafields.json',recipientID,name);
+}
+  
+function getNutri(url,recipientId,name) {
+    console.log(name);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true,"62ffda2cd474e07070563fe4089c443f","6295eda9c2499abe83a342338e7b5562");
     xhr.responseType = 'json';
     xhr.onload = function() {
       var status = xhr.status;
       if (status == 200) {
         
         res = JSON.parse(xhr.responseText);
-        //console.log(res);
-        creationliste(res,recipientId);
-        
-      } else {
-        
-        //callback(status);
-      }
+        console.log(res);
+        var message = res.metafields[0].value 
+        sendTextMessage(recipientId,message);
+      } 
     };
     xhr.send();
 };
 
 
 
-function creationmessage(data,recipientId){
-  console.log(data.products);
+
+
+
+
+
+
+
+
+
+// Fonction qui va créer le message générique 
+function creationmessage(data,recipientId,name){
+  //console.log(data.products);
   var messageData = {
     recipient: {
       id: recipientId
@@ -294,19 +389,19 @@ function creationmessage(data,recipientId){
   var k = 0;
   var message;
   for(var t in data.products){
-    //console.log(data.products[t]);
+    console.log((data.products[t].body_html).italics());
     message = {title: data.products[t].title,
             subtitle: data.products[t].body_html,
-            item_url: "https://fr.ulule.com/chambouletou-pate/",               
+            item_url: "https://tsechatbot.myshopify.com/collections/"+name+"/products/"+data.products[t].handle,               
             image_url: data.products[t].image.src,
             buttons: [{
               type: "web_url",
-              url: "https://fr.ulule.com/chambouletou-pate/",
+              url: "https://tsechatbot.myshopify.com/collections/"+name+"/products/"+data.products[t].handle,
               title: "Ouvrir le site"
             }, {
               type: "postback",
               title: "Plus d'information",
-              payload: "information",
+              payload: "nutri||"+data.products[t].id,
             },
            {
             "type":"postback",
@@ -323,85 +418,12 @@ function creationmessage(data,recipientId){
 
 
 
-function creationliste(data,recipientId){
 
-  var k = 0;
-  var message;
-  var messageData = {
-  "recipient":{
-    "id":recipientId
-  }, "message": {
-    "attachment": {
-        "type": "template",
-        "payload": {
-            "template_type": "list",
-            "elements": [
-            ],
-             "buttons": [
-                {
-                    "title": "Voir Plus",
-                    "type": "web_url",
-                    "url": "https://tsechat.myshopify.com/collections"                        
-                }
-            ]  
-        }
-    }
-}
-    
-};
-  for(var t in data.custom_collections){
-    console.log(data.custom_collections);
-    
-    console.log("\n\n\n\n\n")
-    
-    message =    {
-                    "title": data.custom_collections[k].title,
-                    "image_url": data.custom_collections[k].image.src,
-                    "subtitle": data.custom_collections[k].body_html,
-                    "default_action": {
-                        "type": "web_url",
-                        "url": "https://tsechat.myshopify.com/collections/"+data.custom_collections[k].handle,
-                        
-                    },
-                    "buttons": [{
-                      "title": "Voir plus",
-                    "type": "postback",
-                    "payload": "information||"+ data.custom_collections[k].id,
-                       
-                    }
-                    ]
-                };
-
-    
-    messageData.message.attachment.payload.elements[k] = message;
-    k++;
-  }
-  console.log(messageData);
-  console.log(messageData.message.attachment.payload);
-  callSendAPI(messageData);
-}
-
-
-
-
-
-
-
-function readData(sData) {
-  
-    // On peut maintenant traiter les données sans encombrer l'objet XHR.
-    if (sData == "OK") {
-        console.log("C'est bon");
-      
-    } else {
-        console.log("Y'a eu un problème");
-    }
-}
-
-
+//////////////////////////
+// Fonction va aller sur shopify pour ajouter une commande
+//////////////////////////
 function sendAchatMessage(recipientId) {
-  
- var messageData = {
+  var messageData = {
   "order": {
     "line_items": [
       {
@@ -427,9 +449,7 @@ function sendAchatMessage(recipientId) {
     ]
   }
 }
-  
-  
-callSendAPI(messageData);
+callSendShopify(messageData);
 }
 
 
@@ -437,6 +457,9 @@ callSendAPI(messageData);
 
 
 
+//////////////////////////
+// Fonction qui envoi le message sur Facebook
+//////////////////////////
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
@@ -460,10 +483,14 @@ function callSendAPI(messageData) {
 }
 
 
+//////////////////////////
+// Fonction qui envoi le message sur shopify
+//////////////////////////
 function callSendShopify(messageData) {
   request({
-    uri: 'https://tsechat.myshopify.com/admin/orders.json',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    uri: 'https://tsechatbot.myshopify.com/admin/orders.json',
+    qs: { consumer_key: "62ffda2cd474e07070563fe4089c443f"
+    , consumer_secret: "6295eda9c2499abe83a342338e7b5562"},
     method: 'POST',
     json: messageData
 
@@ -484,13 +511,18 @@ function callSendShopify(messageData) {
 
 
 
-// Set Express to listen out for HTTP requests
+// Mise en route du serveur avec affichage du Greetings Message
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port %s", server.address().port);
   createGetStarted();
 });
 
-function callThreadSettingsAPI(data) { //Thread Reference API
+
+
+//////////////////////////
+// Fonction qui envoi le Thread sur Facebook (Envoi du message Greeting)
+//////////////////////////
+function callThreadSettingsAPI(data) { 
 request({
 uri: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+process.env.token,
 qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
@@ -506,6 +538,9 @@ if (!error && response.statusCode == 200) {
 });  
 }
 
+
+
+// Fonction qui créer le message de Greetings 
 function createGetStarted() {
 var data = {
 setting_type: "call_to_actions",
@@ -516,5 +551,16 @@ call_to_actions:[
 }
 ]
 };
+  
+  var message= {
+  "setting_type":"greeting",
+  "greeting":{
+    
+    "text":"Bienvenue {{user_full_name}} sur le bot de Chamboultou."
+  }
+};
+
+callThreadSettingsAPI(message);
 callThreadSettingsAPI(data);
 }
+
